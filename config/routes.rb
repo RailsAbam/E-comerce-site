0e6700dd488bf5866619515410fe7e4aa69fa217
RailsAbam <railsdevabam.com>
2022-07-22 10:28:27 +0100

    chnages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+  root "home#index"
+  get"home/inverters"
+  get "home/battery"
+  get "home/about_us"
+  get "home/inquiry"
+  get "home/solar"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
